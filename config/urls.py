c021c16a5428a09273e756decee185834df010e5
Landry MMs <mmslandry8@gmail.com>
2026-05-29 06:40:02 +0100

    Configuration frontend et landing page FoodCare

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+
+    # Administration Django
     path('admin/', admin.site.urls),
+
+    # Application principale
+    path('', include('core.urls')),
+
 ]
+
+# Configuration des fichiers media
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+
+def home(request):
+    """
+    Vue de la page d'accueil FoodCare
+    """
+
+    return render(request, 'core/home.html')
